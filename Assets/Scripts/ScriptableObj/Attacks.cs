945b0540c9a9ab81658b2a84c71d928b9e1e6a3f
Big_Train <73400317+Big-Train@users.noreply.github.com>
2026-03-08 14:20:22 +0100

    sprm

--- Assets/Scripts/ScriptableObj/Attacks.cs
+++ Assets/Scripts/ScriptableObj/Attacks.cs
@@ -7,10 +7,14 @@ public class Attacks : ScriptableObject
     public PokemonTypes attackType;
 
     public int pp;
+    public int maxPp;
     public int damage;
     public int accuracy;
     public int speed;
     public string desc;
 
     public Effects effect;
+
+
+
 }
